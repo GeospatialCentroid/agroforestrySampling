9bef5f7da869c4c0b600bfed864121e6380df774
dune <carver.dan1@gmail.com>
2025-09-29 20:45:01 +0000

    added LRR sampling to the NLCD to regions calculations

--- scripts/nlcdToRegions.R
+++ scripts/nlcdToRegions.R
@@ -1,9 +1,8 @@
-pacman::p_load(terra, dplyr,sf, readr, purrr, googledrive, tidyr, stringr )
+pacman::p_load(terra, dplyr,sf, readr, purrr, googledrive, tidyr, stringr, plotly,
+               tictoc)
 
-# extract area counts for each grid of the unique MLRA areas 
-files <- pullAreaFiles(featName = "MLRA", size = 10)
-index <- 1:nrow(files$spatial)
-name <- files$name
+source("functions/samplingWorkflowFunctions.R")
+# grab grid cells 
 grids <- getGrids(gridSize = 10)
 # read in NLCD data 
 nlcdFiles <- list.files("data/derived/nlcdData",
@@ -12,105 +11,148 @@ nlcdFiles <- list.files("data/derived/nlcdData",
                         all.files = TRUE)|>
   terra::rast()
 
+mlra <- TRUE 
 
-# for each MLRA grid 
-for(i in index){
-  # the MLRA boundary 
-  g1 <- files$spatial[i]
-  # get the name object 
-  name <- g1$MLRA_ID[1]
-  # grids within the boundry 
-  #project to AEA for nlcd data 
-  g2 <- prepGrids(grids = grids, subGeo = g1)|>
-    terra::project(crs(nlcdFiles))
-  # crop 
-  n1 <- terra::crop(nlcdFiles, g2) |>
-    terra::mask(g2)
-  # total area 
-  area <- terra::expanse(n1[[1]], unit = "km")[,"area"]
-  nCells <- terra::ncell(n1[[1]])
-  areaPerCell <- area/nCells
-  # extract the values 
-  e1 <- terra::extract(x = nlcdFiles, y = g2)
-  e2 <- e1 |>
-    tidyr::pivot_longer(
-      cols = starts_with("Annual_NLCD"),
-      names_to = "Year",
-      values_to = "Land_Cover_Class"
-      )|>
-    mutate(
-      Year = str_extract(Year, "\\d{4}")
-      )|>
-    dplyr::count(ID, Year, Land_Cover_Class, name = "Count")|>
-    dplyr::mutate(
-      area = Count * areaPerCell
-    )
-  # export the data 
-  write_csv(
-    x = e2, 
-    file = paste0(
-      "data/derived/nlcdSummaryAreas/mlra_",name,".csv"
-    )
-  )
+# working with MLRA and LRR so set some parameters based on that 
+if(isTRUE(mlra)){
+  # extract area counts for each grid of the unique MLRA areas 
+  files <- pullAreaFiles(featName = "MLRA", size = 10)
+  export1 <- "data/derived/nlcdSummaryAreas/allClasses_summaryMLRA.csv"
+  export2 <- "data/derived/nlcdSummaryAreas/mlra_"
+  nameCol <- "MLRA_ID"
+  pattern1 <- "mlra_"
+}else{
+  files <- pullAreaFiles(featName = "LRR", size = 10)
+  export1 <- "data/derived/nlcdSummaryAreas/allClasses_summaryLRR.csv"
+  export2 <- "data/derived/nlcdSummaryAreas/lrr_"
+  nameCol <- "LRR_NAME"
+  pattern1 <- "lrr_"
 }
 
-# summarize the regional features 
-files2 <- list.files(path = "data/derived/nlcdSummaryAreas",
-                     pattern = "mlra_",
-                     full.names = TRUE) 
-for(i in seq_along(files2)){
-  feat <- files2[i]
-  name <- stringr::str_remove(basename(feat), pattern = ".csv")
-  # read and process to full areas
-  t1 <- read_csv(feat) |>
-    group_by(Year, Land_Cover_Class) |>
-    summarize(
-      total_area = sum(area),
-      .groups = 'drop'
-    ) |>
-    mutate(
-      mlra = name
+index <- 1:nrow(files$spatial)
+name <- files$name
+
+if(!file.exists(export1)){
+  # for each MLRA grid 
+  for(i in index){
+    # the MLRA boundary 
+    g1 <- files$spatial[i]
+    # get the name object 
+    name <- g1[[nameCol]][1] |> pull()
+    print(name)
+    # grids within the boundry 
+    #project to AEA for nlcd data 
+    g2 <- prepGrids(grids = grids, subGeo = g1)|>
+      terra::project(crs(nlcdFiles))
+    # crop 
+    n1 <- terra::crop(nlcdFiles, g2) |>
+      terra::mask(g2)
+    # total area 
+    ## this is being a memory hog... just use the vector file 
+    # area <- terra::expanse(n1[[1]], unit = "km")[,"area"]
+    # area <- sum(terra::expanse(g2, unit = "km"), na.rm = TRUE)
+
+    ## direct extraction of values 
+    for(j in 1:3){
+      vals <- terra::values(n1[[j]]) 
+      vals <- vals[!is.na(vals)]
+      if( j == 1){
+        nCells <- length(vals)
+        # areaPerCell <- area/nCells
+        e1 <- data.frame("Annual_NLCD_LndCov_2010"= rep(NA, nCells),
+                         "Annual_NLCD_LndCov_2016" = rep(NA, nCells),
+                         "Annual_NLCD_LndCov_2020" = rep(NA, nCells))
+      }
+      e1[,j] <- vals
+    }
+    # format the data 
+    e2 <- e1 |>
+      tidyr::pivot_longer(
+        cols = starts_with("Annual_NLCD"),
+        names_to = "Year",
+        values_to = "Land_Cover_Class"
+      )|>
+      mutate(
+        Year = str_extract(Year, "\\d{4}")
+      )|>
+      dplyr::count(Year, Land_Cover_Class, name = "Count")
+    # export the data 
+    write_csv(
+      x = e2, 
+      file = paste0(
+        export2,name,".csv"
+      )
     )
-  if(i ==1){
-    results <- t1
-  }else{
-    results <- dplyr::bind_rows(results,t1)
   }
-}
-
-# assign the classes 
-results2 <- results |>
-  dplyr::mutate(
-    class = case_when(
-      Land_Cover_Class == 11 ~ "Open Water",
-      Land_Cover_Class == 12 ~ "Perennial Ice/Snow",
-      Land_Cover_Class == 21 ~ "Developed, Open Space",
-      Land_Cover_Class == 22 ~ "Developed, Low Intensity",
-      Land_Cover_Class == 23 ~ "Developed, Medium Intensity",
-      Land_Cover_Class == 24 ~ "Developed High Intensity",
-      Land_Cover_Class == 31 ~ "Barren Land",
-      Land_Cover_Class == 41 ~ "Deciduous Forest",
-      Land_Cover_Class == 42 ~ "Evergreen Forest",
-      Land_Cover_Class == 43 ~ "Mixed Forest",
-      Land_Cover_Class == 51 ~ "Dwarf Scrub",
-      Land_Cover_Class == 52 ~ "Shrub/Scrub",
-      Land_Cover_Class == 71 ~ "Grassland/Herbaceous",
-      Land_Cover_Class == 72 ~ "Sedge/Herbaceous",
-      Land_Cover_Class == 73 ~ "Lichens",
-      Land_Cover_Class == 74 ~ "Moss",
-      Land_Cover_Class == 81 ~ "Pasture/Hay",
-      Land_Cover_Class == 82 ~ "Cultivated Crops",
-      Land_Cover_Class == 90 ~ "Woody Wetlands",
-      Land_Cover_Class == 95 ~ "Emergent Herbaceous Wetlands"
+  
+  # summarize the regional features 
+  files2 <- list.files(path = "data/derived/nlcdSummaryAreas",
+                       pattern = pattern1,
+                       full.names = TRUE) 
+  
+  
+  
+  for(i in seq_along(files2)){
+    feat <- files2[i]
+    name <- stringr::str_remove(basename(feat), pattern = ".csv")
+    # read and process to full areas
+    t1 <- read_csv(feat)
+    # trying with an assumed area measure of a pixel, where Count is the 
+    # number of individual pixels 
+    t2 <- dplyr::mutate(t1,
+                        calc_area = (Count * 900)/1000000,
+                        areaName = name
     )
-  )
-#export 
-write_csv(x = results2, file = "data/derived/nlcdSummaryAreas/allClasses_summaryMLRA.csv")
+    
+    if(i ==1){
+      results <- t2
+    }else{
+      results <- dplyr::bind_rows(results,t2)
+    }
+  }
+  
+  
+  # assign the classes 
+  results2 <- results |>
+    dplyr::mutate(
+      class = case_when(
+        Land_Cover_Class == 11 ~ "Open Water",
+        Land_Cover_Class == 12 ~ "Perennial Ice/Snow",
+        Land_Cover_Class == 21 ~ "Developed, Open Space",
+        Land_Cover_Class == 22 ~ "Developed, Low Intensity",
+        Land_Cover_Class == 23 ~ "Developed, Medium Intensity",
+        Land_Cover_Class == 24 ~ "Developed High Intensity",
+        Land_Cover_Class == 31 ~ "Barren Land",
+        Land_Cover_Class == 41 ~ "Deciduous Forest",
+        Land_Cover_Class == 42 ~ "Evergreen Forest",
+        Land_Cover_Class == 43 ~ "Mixed Forest",
+        Land_Cover_Class == 51 ~ "Dwarf Scrub",
+        Land_Cover_Class == 52 ~ "Shrub/Scrub",
+        Land_Cover_Class == 71 ~ "Grassland/Herbaceous",
+        Land_Cover_Class == 72 ~ "Sedge/Herbaceous",
+        Land_Cover_Class == 73 ~ "Lichens",
+        Land_Cover_Class == 74 ~ "Moss",
+        Land_Cover_Class == 81 ~ "Pasture/Hay",
+        Land_Cover_Class == 82 ~ "Cultivated Crops",
+        Land_Cover_Class == 90 ~ "Woody Wetlands",
+        Land_Cover_Class == 95 ~ "Emergent Herbaceous Wetlands"
+      )
+    )
+  #export 
+  write_csv(x = results2, file = export1)
+  
+}
 
 
 # Group my categories from NLCD  ------------------------------------------
 # water, develop, barren, forest, shrubland, herbaceous, cul;tivated, wetlands 
-file <- read_csv("data/derived/nlcdSummaryAreas/allClasses_summaryMLRA.csv")
+if(isTRUE(mlra)){
+  file <- read_csv("data/derived/nlcdSummaryAreas/allClasses_summaryMLRA.csv")
+  export3 <- "data/derived/nlcdSummaryAreas/largeClass_summaryMLRA.csv"
+}else{
+  file <- read_csv("data/derived/nlcdSummaryAreas/allClasses_summaryLRR.csv")
+  export3 <- "data/derived/nlcdSummaryAreas/largeClass_summaryLRR.csv"
+}
 
 # assign larger class 
 d1 <- file |>
@@ -123,35 +165,124 @@ d1 <- file |>
     Land_Cover_Class %in% c(71,72,73,74) ~ "Herbaceous",
     Land_Cover_Class %in% c(81,82) ~ "Planted/Cultivated",
     Land_Cover_Class %in% c(90,95) ~ "Wetlands",
-
       )
     )|>
-  group_by(Year,mlra, lcGroup) |>
+  group_by(Year,areaName, lcGroup) |>
   summarize(
-    group_area = sum(total_area ),
+    # group_area = sum(total_area ),
+    group_area = sum(calc_area ),
     .groups = 'drop'
   ) |>
   dplyr::select(
-    Year,mlra,  lcGroup, group_area
-  )
-# get the total area of all MLRA objects 
-mlras <- files$spatial
-mlras$areaKM <- terra::expanse(files$spatial, unit = "km")
-mlrasArea <- mlras |>
-  as.data.frame()|>
-  dplyr::mutate(
-    mlra = paste0("mlra_", as.character(MLRA_ID))
-  )|>
-  dplyr::select(
-    mlra, areaKM
-  )
-# join to group summaries 
-d2 <- dplyr::left_join(d1, mlrasArea, by = "mlra") |>
-  dplyr::mutate(
-    percentArea = (group_area/areaKM)*100
+    Year,areaName,  lcGroup, group_area
   )
 
-# test 
-t1 <- d2 |>
-  group_by(Year, mlra)|>
-  summarize(sum = sum(group_area))
+
+
+# get the total area of all MLRA objects 
+areas <- files$spatial |> 
+  terra::project(nlcdFiles)
+areas$areaKM <- terra::expanse(files$spatial, unit = "km")
+
+
+areasKM <- as.data.frame(areas)[ , c(`nameCol`,"areaKM") ] 
+areasKM$areaName  <- paste0(pattern1, areasKM[[nameCol]])                                              
+# join to group summaries 
+d2 <- dplyr::left_join(d1, areasKM, by = "areaName") |>
+  dplyr::mutate(
+    percentArea = (group_area/areaKM)*100
+  ) |> 
+  dplyr::select(-nameCol)
+
+# export larger group data 
+write_csv(d2, file =export3 )
+
+# # test 
+# t1 <- d2 |>
+#   group_by(Year, areaName)|>
+#   summarize(sum = sum(percentArea))
+# 
+
+
+# generate some plots -----------------------------------------------------
+grabMeanSamples <- function(data){
+  data |>  
+    group_by(id)|>
+    summarise(
+      # Keep the first value found for these columns within each group
+      totalAreas = first(totalAreas),
+      tof = first(tof),
+      # Calculate the average for these columns, removing NAs
+      average_sample = mean(sample, na.rm = TRUE),
+      average_samplePercentage = mean(samplePercentage, na.rm = TRUE))|>
+    dplyr::mutate(
+      average_TOF_perArea = ( (tof/100) /totalAreas) * 100
+    )
+}
+
+
+# Generating plots for the area sample method  ----------------------------
+
+if(isTRUE(mlra)){
+  ## read in the aggregates data values 
+  agg <- read_csv("data/derived/samplingXTesting/aggregated/mlra80_iterations.csv")
+  ag10 <-  read_csv("data/derived/samplingXTesting/aggregated/mlra80_10.csv") |> grabMeanSamples()
+  ag16 <-  read_csv("data/derived/samplingXTesting/aggregated/mlra80_16.csv") |> grabMeanSamples()
+  ag20 <-  read_csv("data/derived/samplingXTesting/aggregated/mlra80_20.csv") |> grabMeanSamples()
+  
+}else{
+  ## read in the aggregates data values 
+  agg <- read_csv("data/derived/samplingXTesting/aggregated/lrr80_iterations.csv")
+  ag10 <-  read_csv("data/derived/samplingXTesting/aggregated/lrr80_10.csv") |> grabMeanSamples()
+  ag16 <-  read_csv("data/derived/samplingXTesting/aggregated/lrr80_16.csv") |> grabMeanSamples()
+  ag20 <-  read_csv("data/derived/samplingXTesting/aggregated/lrr80_20.csv") |> grabMeanSamples()
+  
+}
+
+
+
+producePlots <- function(nlcdData, sampleData, year){
+  # subset to a specific year 
+  df_filtered <- nlcdData |>
+    filter(
+      Year == year
+    )
+  # assign the mlra column to match nlcd data 
+  sampleData$mlra <- paste0("mlra_", sampleData$id)
+  
+  # Create the interactive grouped bar chart
+  fig <- plot_ly(
+    data = df_filtered,
+    x = ~areaName ,
+    y = ~percentArea,
+    color = ~lcGroup,
+    type = 'bar',
+    barmode = 'group' # This groups the bars side-by-side
+  )|>
+    add_trace(
+      data = sampleData,
+      x = ~mlra,
+      y = ~average_samplePercentage, # Use the column from your new data
+      type = 'scatter',
+      mode = 'markers',
+      name = "Sampled Percentage",   # This name will appear in the legend
+      marker = list(color = 'grey', size = 6, symbol = 'diamond'), # Customize the dot
+      inherit = FALSE          # Prevents inheriting properties from the bar chart
+    ) |>
+    layout(
+      title = paste0("NLCD class areas and Sample Percentage - ", year),
+      xaxis = list(title = "MLRA", categoryorder = "category ascending"),
+      yaxis = list(title = "Percent Area (%)", range = c(0, 100)), # Adjust range for text
+      legend = list(title = list(text = "<b> Land Cover Group </b>"))
+    )
+}
+
+p10 <- producePlots(nlcdData = d2, sampleData = ag10,year = "2010")
+p10
+p16 <- producePlots(nlcdData = d2, sampleData = ag16,year = "2016")
+p16
+p20 <- producePlots(nlcdData = d2, sampleData = ag20,year = "2020")
+p20
+
+
+
--- scripts/stratifiedSamplingWorkflow.R
+++ scripts/stratifiedSamplingWorkflow.R
@@ -1,6 +1,6 @@
 
   
-pacman::p_load("terra", "dplyr", "readr", "sf", "tictoc", "tmap")
+pacman::p_load("terra", "dplyr", "readr", "sf", "tictoc", "tmap", "plotly", "broom", "rlang", "leaflet")
 tmap_mode("view")
 source("functions/samplingWorkflowFunctions.R")
 # goal 
@@ -19,12 +19,47 @@ featName <- "MLRA"
 size <- "10k"
 proportionValue <-  80
 
+
+# generate a leafetmap for visulizaitons  ---------------------------------
+mlra <- getSpatialFiles(area = "MLRA")
+lrr <- getSpatialFiles(area = "LRR")
+map <- leaflet() %>%
+  # --- Add Background Map Layers (Base Groups) ---
+  addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
+  addTiles(group = "OpenStreetMap") %>% # OSM is the default tile layer
+  # --- Add Polygon Layers (Overlay Groups) ---
+  addPolygons(
+    data = mlra,
+    color = "#0073C2FF",  # A distinct blue outline color
+    weight = 2,           # Outline thickness
+    fillOpacity = 0.5,
+    group = "MLRA",  # Assign to a group for the toggle control
+    label = ~MLRA_ID  # Text that appears on hover
+  ) %>%
+  addPolygons(
+    data = lrr,
+    color = "#EFC000FF",  # A distinct gold outline color
+    weight = 2,
+    fillOpacity = 0,
+    group = "LRR", # Assign to a different group
+    label = ~LRR_NAME 
+  ) %>%
+  # --- Add the Layer Control Widget ---
+  addLayersControl(
+    baseGroups = c("OpenStreetMap", "Satellite"),
+    overlayGroups = c("MLRA", "LRR"),
+    options = layersControlOptions(collapsed = FALSE) # Keep the control open
+  )
+map
+
+
+
 # results at 80% agreement  ------------------------------------------------
 ## with 5,10,20 itorations of j : 25,43,78
 ## with 100 itorations of j : 76.915 sec elapsed
 time <- c()
-inter <- c(5,10,200,500,1000)
-for(itor in 1:5){
+inter <- c(5000, 10000)
+for(itor in 1:2){
   print(itor)
   tic()
   mlra80 <- purrr::map(.x = featureAreas[1], .f = callStartitfied, size = "10k",
@@ -38,6 +73,143 @@ for(itor in 1:5){
   export <- paste0("data/derived/samplingXTesting/mlra80_",inter[itor],".csv")
   write_csv(x = mlra80[[1]], export)
 }
+
+
+# compile to single feature  ----------------------------------------------
+f1 <- list.files("data/derived/samplingXTesting",
+                 pattern = "80_",
+                 full.names = TRUE) 
+# read in assign sample amount and combine 
+for(i in seq_along(f1)){
+  feat <- f1[i]
+  base <- tools::file_path_sans_ext(basename(feat))|> 
+    stringr::str_remove( "mlra80_")
+  d1 <- read_csv(feat)|>
+    dplyr::mutate(iterations = base)
+  if(i == 1){
+    result <- d1
+  }else{
+    result <- bind_rows(result, d1)
+  }
+}
+# alter values to percent of areas rather than counts 
+result1 <- result |> 
+  mutate(
+    sample10_percent = (sample10 / totalAreas) * 100,
+    sample16_percent = (sample16 / totalAreas) * 100,
+    sample20_percent = (sample20 / totalAreas) * 100,
+    iterations = as.numeric(iterations)
+  )
+
+
+write_csv(result1, "data/derived/samplingXTesting/aggregated/mlra80_iterations.csv")
+
+# parse out results in to csv per year  -----------------------------------
+select_by_suffix <- function(data, suffix) {
+  sel <- data %>%
+    select(id, totalAreas,iterations, contains(as.character(suffix)))
+  names(sel) <- c("id", "totalAreas","iterations", "tof", "sample", "sampleTOF", "samplePercentage")
+  return(sel)
+}
+
+for(i in c("10","16","20")){
+  export <- paste0("data/derived/samplingXTesting/aggregated/mlra80_",i,".csv")
+  sel <- select_by_suffix(data = result1, suffix = i)
+  write_csv(x = sel, file = export)
+}
+
+# generate the plot wit the lms  ------------------------------------------
+create_lm_fit <- function(data, response_col) {
+  
+  # The '{{}}' operator allows us to use the response_col argument
+  # as a variable for the column name.
+  
+  data %>%
+    # We rename the column provided in response_col to a temporary,
+    # consistent name 'response'. This makes the lm() formula simple.
+    rename(response = {{ response_col }}) %>%
+    
+    # Now we can filter and model using that consistent name
+    filter(!is.na(response)) %>%
+    group_by(id) %>%
+    do(augment(lm(response ~ iterations, data = .)))
+}
+
+
+
+
+# add a lm for plot 
+result2 <-create_lm_fit(data = result1, response_col = "sample10_percent")
+  
+
+
+# Define the plotting function
+plot_trends <- function(marker_data,  y_var) {
+  
+  # --- Step 1: Prepare variables for plotting ---
+  # Capture the column name expression
+  y_var_expr <- enquo(y_var)
+  
+  # Convert it to a string for use in titles
+  y_var_string <- as_name(y_var_expr)
+  
+  
+  # generate the lm 
+  line_data <-create_lm_fit(data = marker_data, response_col = y_var_string)
+  
+  
+  
+  # --- Step 2: Build the plot ---
+  fig <- plot_ly() %>%
+    add_trace(
+      data = marker_data,
+      x = ~iterations,
+      # Use the captured expression for the y-axis
+      y = y_var_expr, 
+      color = ~factor(id),
+      type = 'scatter',
+      mode = 'markers',
+      legendgroup = ~factor(id)
+    ) %>%
+    add_trace(
+      data = line_data,
+      x = ~iterations,
+      y = ~.fitted, # The fitted line data always uses '.fitted'
+      color = ~factor(id),
+      type = 'scatter',
+      mode = 'lines',
+      legendgroup = ~factor(id),
+      showlegend = FALSE
+    ) %>%
+    # --- Step 3: Customize the layout with dynamic titles ---
+    layout(
+      title = paste(y_var_string, "vs. Iterations by ID"),
+      xaxis = list(title = "Iterations"),
+      yaxis = list(title = y_var_string),
+      legend = list(title = list(text = "<b> ID </b>"))
+    )
+  
+  # Return the final plot object
+  return(fig)
+}
+#2010
+plot_trends(marker_data = result1,
+            y_var = sample10_percent)
+#2016
+plot_trends(marker_data = result1,
+            y_var = sample20_percent)
+#2020
+plot_trends(marker_data = result1,
+            y_var = sample16_percent)
+
+# summary
+## there are three locations (150,146,142) where the sample density is around 20% 
+## 63 with only 4 locations require a full sample 
+## The rest hover between 40 and 80, with 87,80 being very consistent across the years 
+## possible group of  80, 77, 88, 79,146 
+## possible group of 89 78 81 72 87 
+
+
 
 # tic()
 # mlra80 <- purrr::map(.x = featureAreas[1], .f = callStartitfied, size = "10k", proportionValue =  80, .progress = TRUE)
